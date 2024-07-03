2023ae27b0058ade276a427f08ab811f9e5a670c
Guillaume Charpentier <guillaume.prodancer@gmail.com>
2024-07-03 13:41:56 +0200

    Création du repo et ajout des fleches

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+/* ligne directive:
+
+ajouts des differents let et const (dots et arrow left&right)
+ajouter code pour remplir et déplacer les dots
+rendre les fleches clickables
+*/
