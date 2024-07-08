d575f35647a0e1b4ec573692932487e6e05a55d5
Guillaume Charpentier <guillaume.prodancer@gmail.com>
2024-07-08 17:07:26 +0200

    ajout des dots avec JS + descriptions des actions

--- assets/script.js
+++ assets/script.js
@@ -17,25 +17,46 @@ const slides = [
 	}
 ]
 
+//currentSlide avec i fixé à zéro (1)
+let currentSlide = 0;
 
-// Variables
-
-//constante pour mes images de la bannière
-const image = document.querySelector('.banner-img');
-//constante pour mes textes accompagnants les images de la bannière
+//constante pour mes images dans la bannière
+const imgBanner = document.querySelector('.banner-img');
+//constante pour les taglines
 const tagLine = document.querySelector('#banner p');
-//constantes pour les bullets pointers//
-const dotsPart = document.querySelector('.dots');
-//constantes pour mes flèches gauches et droites//
+//constante pour les bullet pointer
+const dotsContainer = document.querySelector('.dots');
+//constante pour les flèches gauche et droite
 const rightArrow = document.querySelector('.arrow_right');
 const leftArrow = document.querySelector('.arrow_left');
 
-////////////////////////////////////////////////
+// Ajout des dots //
 
-rightArrow.addEventListener("click", () => {
-	//console.log("click droit");
-})
+//boucle forEach (callback function) pour parcourir chaque élément du array (tableau 'slides')
+//underscore représente chaque diapositive dans slides, mais je n'utilise pas cette information directement dans la boucle.
+//Au lieu de l'appeler slides ou element, je l'appelle '_' pour indiquer que ce paramètre est intentionnellement ignoré.
+// i (index) est utilisé pour créer les points de navigation et les associer aux diapositives.
+//création d'une div 'dot' (présente dans mon css)
+//class.list.add 'dot' pour ajouter cette classe à mon code
+// si i égale zéro, ajout de la classe 'dot_selected' et donc dot plein
 
-leftArrow.addEventListener("click", () => {
-	//console.log("click gauche");
-})
+slides.forEach((_, i) => {
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    if (i === 0) dot.classList.add('dot_selected');
+    dot.addEventListener('click', () => {
+        currentSlide = i;
+    });
+    dotsContainer.appendChild(dot);
+});
+
+// Ajoute des écouteurs d'événements aux flèches
+rightArrow.addEventListener('click', () => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    console.log('click droit');
+});
+
+leftArrow.addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    console.log('click gauche');
+});
